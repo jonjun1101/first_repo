4006f5476276066d2a2939f1c4dd971234eefa23
jonjun1101 <jonasjunker@gmx.de>
2025-01-24 14:24:37 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -20,6 +20,6 @@ contrast = ((max - offset) + (min - offset)) / ((max - offset) - (min - offset))
 st.write(f"The equivalent temperature in Fahrenheit is: **{contrast:.2f}°F**")
 
 # Add an optional slider for fun
-st.subheader("Play around with the Celsius slider")
-celsius_slider = st.slider("Celsius", min_value=-100, max_value=100, value=0)
-st.write(f"Slider value in Fahrenheit: {celsius_slider * 9/5 + 32:.2f}°F")
+#st.subheader("Play around with the Celsius slider")
+#celsius_slider = st.slider("Celsius", min_value=-100, max_value=100, value=0)
+#st.write(f"Slider value in Fahrenheit: {celsius_slider * 9/5 + 32:.2f}°F")
